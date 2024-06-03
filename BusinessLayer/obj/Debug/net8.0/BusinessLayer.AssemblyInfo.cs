aabdac579779a66a033ff41bead2742a77d0f518
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-06-03 09:43:22 +0530

    update kafka

--- BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f7814211077f8dd8674568e2c2dbb9965914f50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efc67618b2a403e5a0a7bb240e40c6385d54b893")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RepositoryLayer/obj/Debug/net8.0/RepositoryLayer.AssemblyInfo.cs
+++ RepositoryLayer/obj/Debug/net8.0/RepositoryLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f7814211077f8dd8674568e2c2dbb9965914f50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efc67618b2a403e5a0a7bb240e40c6385d54b893")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
